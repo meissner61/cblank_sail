3ac26b637a1270be10f8955d0824a308f863067f
Vlad <vlad.lopatinsky@gmail.com>
2024-08-14 12:56:04 -0700

    Added a input system to move the ship around the screen

--- src/main.c
+++ src/main.c
@@ -15,6 +15,8 @@
 void PrepareScene(void);
 void PresentScene(void);
 void DoInput(void);
+void DoKeyDown(SDL_KeyboardEvent* l_event);
+void DoKeyUp(SDL_KeyboardEvent* l_event);
 void Blit(SDL_Texture* texture, int x, int y);
 
 
@@ -49,11 +51,31 @@ int main(int argc, char* argv[])
 
         DoInput();
 
+        if (app.up)
+        {
+            player.y -= 4;
+        }
+
+        if (app.down)
+        {
+            player.y += 4;
+        }
+
+        if (app.left)
+        {
+            player.x -= 4;
+        }
+
+        if (app.right)
+        {
+            player.x += 4;
+        }
+
         Blit(player.texture, player.x, player.y);
 
         PresentScene();
 
-        //SDL_Delay(16);
+        SDL_Delay(16);
     }
 
     return 0;
@@ -67,9 +89,6 @@ void PrepareScene(void)
 
 void PresentScene(void)
 {
-
-    //SDL_RenderCopy(app.renderer, player.texture, NULL, NULL);
-
     SDL_RenderPresent(app.renderer);
 }
 
@@ -82,11 +101,69 @@ void DoInput(void)
         switch(event.type)
         {
             case SDL_QUIT:
+            {
                 exit(0);
                 break;
+            }
+            case SDL_KEYDOWN:
+            {
+                DoKeyDown(&event);
+                break;
+            }
+            case SDL_KEYUP:
+            {
+                DoKeyUp(&event);
+                break;
+            }
 
             default:
                 break;
+        }
+    }
+}
+
+void DoKeyDown(SDL_KeyboardEvent *l_event)
+{
+    if(l_event->repeat == 0)
+    {
+        if(l_event->keysym.scancode == SDL_SCANCODE_W)
+        {
+            app.up = 1;
+        }
+        if(l_event->keysym.scancode == SDL_SCANCODE_S)
+        {
+            app.down = 1;
+        }
+        if(l_event->keysym.scancode == SDL_SCANCODE_D)
+        {
+            app.right = 1;
+        }
+        if(l_event->keysym.scancode == SDL_SCANCODE_A)
+        {
+            app.left = 1;
+        }
+    }
+}
+
+void DoKeyUp(SDL_KeyboardEvent *l_event)
+{
+    if(l_event->repeat == 0)
+    {
+        if(l_event->keysym.scancode == SDL_SCANCODE_W)
+        {
+            app.up = 0;
+        }
+        if(l_event->keysym.scancode == SDL_SCANCODE_S)
+        {
+            app.down = 0;
+        }
+        if(l_event->keysym.scancode == SDL_SCANCODE_D)
+        {
+            app.right = 0;
+        }
+        if(l_event->keysym.scancode == SDL_SCANCODE_A)
+        {
+            app.left = 0;
         }
     }
 }
